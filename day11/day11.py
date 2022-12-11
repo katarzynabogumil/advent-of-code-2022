b907fb42dda791ab7e01710b9d275d72a85b8b7e
katarzynabogumil <katarzyna.bogumil@gmail.com>
2022-12-11 14:12:54 +0100

    day 11

---
+++ day11/day11.py
@@ -0,0 +1,95 @@
+class Monkey:
+  def __init__(self, func, func_variable, devisible_test_value, a, b):
+    self.items = []
+    self.operation = func
+    self.operation_var = func_variable
+    self.test = devisible_test_value
+    self.test_true = a
+    self.test_false = b
+    self.counter = 0
+
+
+def main():
+  monkeys = set_monkeys()
+
+  magic_divide = 1
+  for monkey in monkeys.values():
+    magic_divide *= monkey.test
+
+  round_limit = 10000 # 20 for part 1
+  rounds = 0
+  while rounds < round_limit:
+    for monkey in monkeys.values():
+      for n in range(len(monkey.items)):
+        inspect(monkeys, monkey, magic_divide)
+    rounds +=1
+
+  counters = [monkey.counter for monkey in monkeys.values()]
+  counters.sort()
+  max_counter = counters.pop()
+  max2_counter = counters.pop()
+  
+  print(max_counter * max2_counter)
+
+
+def inspect(monkeys, monkey, magic_divide):
+  item = monkey.items[0]
+  item = monkey.operation(item, monkey.operation_var)
+  # item = item // 3 # for part 1
+  item = item % magic_divide
+
+  if item % monkey.test == 0:
+    monkey.items.pop(0)
+    receiving_monkey = monkey.test_true
+    monkeys[receiving_monkey].items.append(item)
+  else:
+    monkey.items.pop(0)
+    receiving_monkey = monkey.test_false
+    monkeys[receiving_monkey].items.append(item)
+
+  monkey.counter += 1
+
+
+def set_monkeys():
+  monkeys = {}
+
+  with open('input_data.txt', 'r') as input_data:
+    rows = [line.strip() for line in input_data.readlines()]
+
+  for i, row in enumerate(rows):
+    if row.startswith('Monkey'):
+      monkey_index = int(row[-2])
+      starting_items = rows[i + 1][16:].split(', ')
+
+      operation_line = rows[i + 2]
+      try:
+        operation_variable = int(operation_line.split(' ')[-1])
+      except ValueError:
+        operation_variable = None
+
+      if operation_line.endswith('old * old'):
+        def operation(x, y):
+          return x * x
+      elif '*' in operation_line:
+        def operation(x, y):
+          return x * y
+      else:
+        def operation(x, y):
+          return x + y
+
+      test_value = int(rows[i + 3].split(' ')[-1])
+      test_true = int(rows[i + 4].split(' ')[-1])
+      test_false = int(rows[i + 5].split(' ')[-1])
+
+      monkey = Monkey(operation, operation_variable, test_value, test_true, test_false)
+      for n in range(len(starting_items)):
+        monkey.items.append(int(starting_items[n]))
+
+      monkeys[monkey_index] = monkey
+
+  return monkeys
+
+
+if __name__ == "__main__":
+    main()
+
